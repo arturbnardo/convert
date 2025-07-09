fa1b07eba7e0ce2e601f5862f05483156d24301a
Artur Bernardo <arturbrnd1@gmail.com>
2025-07-08 23:40:48 -0300

    currency format fixed

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // Cotação de moedas do dia.
-const USD = 5.21
-const EUR = 5.87
-const GBP = 6.34
+const USD = 5.45
+const EUR = 6.39
+const GBP = 7.40
 
+// Obtendo os elementos do formulário.
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -33,5 +37,40 @@ form.onsubmit = (event) => {
 // Função para converter a moeda.
 
 function convertCurrency(amount, price, symbol){
-  console.log(amount, price, symbol)
+  try {
+    // Exibindo a cotação da moeda selecionada.
+    description.textContent = `${symbol}1 = ${formatCurrencyBRL(price)}`
+    
+    // Calcula o total. 
+    let total = amount * price
+
+    // Verifica se o resultado não é um número.
+    if(isNaN(total)){
+      return alert("Por favor, digite o valor corretamente para converter")
+    }
+
+    // Formatando o valor total.
+    total = formatCurrencyBRL(total).replace("R$", "")
+    
+    // Exibe o resultado total.
+    result.textContent = total = `${total} Reais`
+
+    // Adiciona a classe que mostra o resultado da conversão.
+    footer.classList.add("show-result")
+  } catch (error) {
+    // Remove a classe que mostra o resultado da conversão.
+    footer.classList.remove("show-result")
+    
+    console.log(error)
+    alert("Não foi possivel converter. Tente novamente mais tarde.")
+  }
+}
+
+// Formata a moeda em Real Brasileiro.
+function formatCurrencyBRL(value){
+  // Converte para número para utilizar o toLocaleString para formatar no padrão BRL (R$ 00,00).
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+  })
 }
